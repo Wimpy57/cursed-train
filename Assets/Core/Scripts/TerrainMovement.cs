85096c8ab3502dade90e610944c09a08645b7236
Wimpy57 <venigretdj@gmail.com>
2025-05-31 22:36:49 +0300

    hui

--- Assets/Core/Scripts/TerrainMovement.cs
+++ Assets/Core/Scripts/TerrainMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
 
 namespace Core.Scripts
 {
@@ -7,23 +8,56 @@ namespace Core.Scripts
         [SerializeField] private GameObject TerrainModel;
     
         [SerializeField] private Transform SpawnLocation;
+        [SerializeField] private Transform OffsetSpawnLocation;
+        
 
         [SerializeField] private float Velocity;
         
         private float _timer = 0f;
-        private GameObject _currentTerrainModel;
+        private float _TimeToTravel;
+        private GameObject _firstTerrainModel;
+        private GameObject _secondTerrainModel;
+        private float _width;
+        private bool _firstTime = true;
 
+        
+
+
+        void Start()
+        {
+            _width = 1000;
+            _TimeToTravel = _width / Velocity;
+            
+            
+        }
   
 
         // Update is called once per frame
         void Update()
         {
             if (_timer <= 0f)
-            {            
-                Destroy(_currentTerrainModel);         
-                _currentTerrainModel = Instantiate(TerrainModel, SpawnLocation);
-                _currentTerrainModel.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, Velocity);
-                _timer = 180f/Velocity;
+            {
+                if (_firstTime)
+                {
+                    _firstTerrainModel = Instantiate(TerrainModel, OffsetSpawnLocation);
+                    _firstTerrainModel.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, Velocity);
+                    _secondTerrainModel = Instantiate(TerrainModel, SpawnLocation);
+                    _secondTerrainModel.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, Velocity);
+                    
+                    _firstTime = false;
+                }
+                else
+                {
+                    _secondTerrainModel = _firstTerrainModel;
+                    _firstTerrainModel = Instantiate(TerrainModel, OffsetSpawnLocation);
+                    _firstTerrainModel.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, Velocity);
+                }
+                _timer = _TimeToTravel;
+            }
+
+            if ((_TimeToTravel - _timer) * Velocity >= 360f)
+            {
+                Destroy(_secondTerrainModel);
             }
 
             _timer -= Time.deltaTime;
